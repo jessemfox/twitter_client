0b5ec60892011e9c3b57eb010c20f57c1ad88b51
App Academy Student <student@appacademy.io>
2014-02-17 15:53:35 -0800

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TweeterBS::Application.config.secret_key_base = 'eeb604f89046f2e3d4ceaede614ecf134d6c6ff33115d2e8c36b14ef71c3cb3afd738d37f0c9ccb5f452d270533558e69700ddd79b632d3e40b0f1f78e4fe22a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TweeterBS::Application.config.session_store :cookie_store, key: '_TweeterBS_session'
